8734f778188c2990fbaae39f4ebc2600651cc139
IanTorquato <iantorquato2@gmail.com>
2021-02-24 09:05:07 -0300

    ChallengBox when inactive challenge

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+function ChallengeBox() {
+	return (
+		<div className={styles.challengeBoxContainer}>
+			<div className={styles.challengeNotActive}>
+				<strong>Finalize um ciclo para receber um desafio</strong>
+
+				<p>
+					<img src="icons/level-up.svg" alt="Level Up" />
+
+					Avance de level completando desafios.
+				</p>
+			</div>
+		</div>
+	)
+}
+
+export { ChallengeBox }
