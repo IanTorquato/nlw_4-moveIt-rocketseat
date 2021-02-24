ea6ac9ffc07dd61b24c1574611c0ee07549141a9
IanTorquato <iantorquato2@gmail.com>
2021-02-24 10:21:13 -0300

    spaces from tabsize + add Challenges.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,11 @@ import { ChallengeProvider } from '../contexts/Challenges'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengeProvider>
-      <Component {...pageProps} />
-    </ChallengeProvider>
-  )
+	return (
+		<ChallengeProvider>
+			<Component {...pageProps} />
+		</ChallengeProvider>
+	)
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,27 +9,27 @@ import { ChallengeBox } from '../components/ChallengeBox'
 import styles from '../styles/pages/Home.module.css'
 
 function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Início | move.it</title>
+			</Head>
 
-      <ExperienceBar />
+			<ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
+			<section>
+				<div>
+					<Profile />
+					<CompletedChallenges />
+					<Countdown />
+				</div>
 
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
-    </div>
-  )
+				<div>
+					<ChallengeBox />
+				</div>
+			</section>
+		</div>
+	)
 }
 
 export default Home
