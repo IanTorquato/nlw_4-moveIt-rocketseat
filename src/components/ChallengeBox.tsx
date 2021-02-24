619005fe5aedcdada2084af2ee918b4fa6814ae2
IanTorquato <iantorquato2@gmail.com>
2021-02-24 09:31:37 -0300

    ChallengBox when active challenge

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,17 +1,43 @@
 import styles from '../styles/components/ChallengeBox.module.css'
 
 function ChallengeBox() {
+	const hasActiveChallenge = true
+
 	return (
 		<div className={styles.challengeBoxContainer}>
-			<div className={styles.challengeNotActive}>
-				<strong>Finalize um ciclo para receber um desafio</strong>
+			{hasActiveChallenge ? (
+				<div className={styles.challengeActive}>
+					<header>Ganhe 400 xp</header>
 
-				<p>
-					<img src="icons/level-up.svg" alt="Level Up" />
+					<main>
+						<img src="icons/body.svg" />
+						<strong>Novo desafio</strong>
+						<p>Levante e faça uma caminhada de 3 minutos.</p>
+					</main>
+
+					<footer>
+						<button className={styles.challengeFailedButton} type="button"
+							onClick={() => { }}>
+							Falhei
+						</button>
+
+						<button className={styles.challengeSucceededButton} type="button"
+							onClick={() => { }}>
+							Completei
+						</button>
+					</footer>
+				</div>
+			) : (
+					<div className={styles.challengeNotActive}>
+						<strong>Finalize um ciclo para receber um desafio</strong>
+
+						<p>
+							<img src="icons/level-up.svg" alt="Level Up" />
 
 					Avance de level completando desafios.
 				</p>
-			</div>
+					</div>
+				)}
 		</div>
 	)
 }
