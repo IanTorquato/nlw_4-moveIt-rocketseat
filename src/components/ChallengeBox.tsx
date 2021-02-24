92cba4f4dfe9e5de6a709a75324067177be6a309
IanTorquato <iantorquato2@gmail.com>
2021-02-24 10:34:09 -0300

    Select random challenge

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,18 +1,22 @@
+import { useContext } from 'react'
+
+import { ChallengeContext } from '../contexts/Challenges'
+
 import styles from '../styles/components/ChallengeBox.module.css'
 
 function ChallengeBox() {
-	const hasActiveChallenge = true
+	const { activeChallenge } = useContext(ChallengeContext)
 
 	return (
 		<div className={styles.challengeBoxContainer}>
-			{hasActiveChallenge ? (
+			{activeChallenge ? (
 				<div className={styles.challengeActive}>
-					<header>Ganhe 400 xp</header>
+					<header>Ganhe {activeChallenge.amount} xp</header>
 
 					<main>
-						<img src="icons/body.svg" />
+						<img src={`icons/${activeChallenge.type}.svg`} />
 						<strong>Novo desafio</strong>
-						<p>Levante e faça uma caminhada de 3 minutos.</p>
+						<p>{activeChallenge.description}</p>
 					</main>
 
 					<footer>
--- src/contexts/Challenges.tsx
+++ src/contexts/Challenges.tsx
@@ -1,9 +1,18 @@
 import { createContext, ReactNode, useState } from 'react'
 
+import challenges from '../../challenges.json'
+
+interface Challenge {
+	type: 'body' | 'eye'
+	description: string
+	amount: number
+}
+
 interface ChallengeContextData {
 	level: number
 	currentExperience: number
 	challengeCompleted: number
+	activeChallenge: Challenge
 	startNewChallenge: () => void
 }
 
@@ -17,14 +26,18 @@ function ChallengeProvider({ children }: ChallengeProviderProps) {
 	const [level, setLevel] = useState(1)
 	const [currentExperience, setCurrentExperience] = useState(0)
 	const [challengeCompleted, setChallengeCompleted] = useState(0)
+	const [activeChallenge, setActiveChallenge] = useState(null)
 
 	function startNewChallenge() {
-		console.log("New Challenge")
+		const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+		const challenge = challenges[randomChallengeIndex]
+
+		setActiveChallenge(challenge)
 	}
 
 	return (
 		<ChallengeContext.Provider value={
-			{ level, currentExperience, challengeCompleted, startNewChallenge }
+			{ level, currentExperience, challengeCompleted, activeChallenge, startNewChallenge }
 		}>
 			{children}
 		</ChallengeContext.Provider>
