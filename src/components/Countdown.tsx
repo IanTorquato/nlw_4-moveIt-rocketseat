89167fc690ed21c8bf302a507a62576c8c133efb
IanTorquato <iantorquato2@gmail.com>
2021-02-24 08:41:21 -0300

    Stop and Reset Countdown :clock:

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from 'react'
 
 import styles from '../styles/components/Countdown.module.css'
 
+let countdownTimeout: NodeJS.Timeout
+
 function Countdown() {
-	const [time, setTime] = useState(25 * 60)
-	const [active, setActive] = useState(false)
+	const [time, setTime] = useState(0.05 * 60)
+	const [isActive, setIsActive] = useState(false)
+	const [hasFinished, setHasFinished] = useState(false)
 
 	const minutes = Math.floor(time / 60)
 	const seconds = time % 60
@@ -13,16 +16,25 @@ function Countdown() {
 	const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
 	function startCountdown() {
-		setActive(true)
+		setIsActive(true)
+	}
+
+	function resetCountdown() {
+		clearTimeout(countdownTimeout)
+		setIsActive(false)
+		setTime(0.05 * 60)
 	}
 
 	useEffect(() => {
-		if (active && time > 0) {
-			setTimeout(() => {
+		if (isActive && time > 0) {
+			countdownTimeout = setTimeout(() => {
 				setTime(time - 1)
 			}, 1000)
+		} else if (isActive && time === 0) {
+			setHasFinished(true)
+			setIsActive(false)
 		}
-	}, [active, time])
+	}, [isActive, time])
 
 	return (
 		<div>
@@ -40,10 +52,27 @@ function Countdown() {
 				</div>
 			</div>
 
-			<button className={styles.countdownButton} type="button"
-				onClick={startCountdown}>
-				Iniciar um ciclo
-			</button>
+			{hasFinished ? (
+				<button className={styles.countdownButton} disabled>
+					Ciclo encerrado
+				</button>
+			) : (
+					<>
+						{
+							isActive ? (
+								<button className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+									type="button" onClick={resetCountdown}>
+									Abandonar ciclo
+								</button>
+							) : (
+									<button className={styles.countdownButton} type="button"
+										onClick={startCountdown}>
+										Iniciar um ciclo
+									</button>
+								)
+						}
+					</>
+				)}
 		</div>
 	)
 }
